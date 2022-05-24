e99e4aff363d3b8e5c1103326b99b3f4c311f898
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2022-05-24 13:30:47 +0200

    update

---
+++ docs/shortName/metadata.js
@@ -0,0 +1,14 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specType: "voorbeeld-sjabloon", // vrije tekst
+  specStatus: "IR", // kies uit https://github.com/Stichting-CROW/respec/wiki/specStatus
+  imprint: "crow", // Kies uit http://docs.crow.nl/respec-design/stichting-crow/#logos
+  shortName: "repository-naam/shortName",
+  editors: [
+    {
+      name: "Auteur van der Achternaam", // verplicht, de rest is optioneel
+      company: "CROW",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/repository-naam",
+};
